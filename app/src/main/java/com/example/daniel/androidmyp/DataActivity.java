fdd707f2e270c49ebeabf1ef5cb01dbbe35495cb
DanWilliams24 <01danielwilliams@gmail.com>
2018-03-03 12:22:45 -0500

    Stripped down app to basic Data and Login Activity. The Login activity is currently inactive.

--- app/src/main/java/com/example/daniel/androidmyp/DataActivity.java
+++ app/src/main/java/com/example/daniel/androidmyp/DataActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -14,54 +15,27 @@ import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class DataActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    private final String url = "http://localhost:3000/grades";
+    private final String url = "http://10.0.2.2:3000/grades";
     private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
-        mAuth = FirebaseAuth.getInstance();
         requestQueue = Volley.newRequestQueue(this);
 
     }
 
 
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
-    }
-
-
-    private void updateUI(FirebaseUser user){
-        if(user != null){
-           //User found:
-            // Load api data
-            loadAPIData();
-            // Go ahead and load userData
-
-
-        } else {
-            //If user is not signed in, go back to sign-in
-            moveToView();
-        }
-    }
-
-    private void moveToView() {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-
-    }
-
-
     private void loadAPIData(){
         StringRequest request = new StringRequest(Request.Method.GET, url, onCoursesLoaded, onCoursesError);
+
+
         requestQueue.add(request);
     }
 
@@ -79,5 +53,7 @@ public class DataActivity extends AppCompatActivity {
             Log.e("PostActivity", error.toString());
         }
     };
+
+
 
 }
