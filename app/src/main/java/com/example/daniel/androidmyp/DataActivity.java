d578d610aa1ca54382eb90da1ee73e17eda93b1f
DanWilliams24 <01danielwilliams@gmail.com>
2018-03-03 13:16:40 -0500

    Finished MCPS course view feature

--- app/src/main/java/com/example/daniel/androidmyp/DataActivity.java
+++ app/src/main/java/com/example/daniel/androidmyp/DataActivity.java
@@ -1,9 +1,17 @@
 package com.example.daniel.androidmyp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -14,19 +22,32 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DataActivity extends AppCompatActivity {
     private final String url = "http://10.0.2.2:3000/grades";
     private RequestQueue requestQueue;
+    private ListView listView;
+    private Gson gson;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
         requestQueue = Volley.newRequestQueue(this);
+        listView = findViewById(R.id.data_list_view);
+        loadAPIData();
+
+        //Initialize our GSON object
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gson = gsonBuilder.create();
 
     }
 
@@ -35,7 +56,6 @@ public class DataActivity extends AppCompatActivity {
     private void loadAPIData(){
         StringRequest request = new StringRequest(Request.Method.GET, url, onCoursesLoaded, onCoursesError);
 
-
         requestQueue.add(request);
     }
 
@@ -43,7 +63,22 @@ public class DataActivity extends AppCompatActivity {
     private final Response.Listener<String> onCoursesLoaded = new Response.Listener<String>() {
         @Override
         public void onResponse(String response) {
-            Log.i("PostActivity", response);
+
+            List<Course> classes = Arrays.asList(gson.fromJson(response, Course[].class));
+            Log.i("PostActivity", classes.size() + " courses loaded.");
+            for(Course course: classes){
+                Log.i("PostActivity", "Name: " + course.name);
+            }
+
+            String[] listItems = new String[classes.size()];
+            for (int i = 0; i < classes.size(); i++){
+                Course course = classes.get(i);
+                listItems[i] = course.name;
+            }
+
+            ArrayAdapter adapter = createAdaptor(android.R.layout.simple_list_item_1, listItems);
+            listView.setAdapter(adapter);
+
         }
     };
 
@@ -55,5 +90,8 @@ public class DataActivity extends AppCompatActivity {
     };
 
 
+    private ArrayAdapter createAdaptor(int id, String[] items){
+        return new ArrayAdapter(this,id,items);
 
+    }
 }
